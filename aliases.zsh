f5f205dead17624786c13bf7a5546cb15c76e600
Alex Garrett-Smith <alex@codecourse.com>
2021-06-11 12:43:23 +0100

    Added git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -6,3 +6,12 @@ alias a="php artisan"
 
 # JS
 alias watch="npm run watch"
+
+# Git
+alias gst="git status"
+alias gc="git checkout"
+alias gb="git branch"
+alias commit="git add . && git commit -m"
+alias stash="git stash -u"
+alias push="git push"
+alias pull="git pull"
